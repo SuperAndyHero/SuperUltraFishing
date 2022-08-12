3bcc21283118661cb6ca495db91e42e95536dd9a
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2022-08-12 03:20:41 -0500

    Added fastnoise and started on pool checking

--- World.cs
+++ World.cs
@@ -37,6 +37,77 @@ namespace SuperUltraFishing
             WorldGenerated = false;
         }
 
+        private bool PassThough(Tile tile) =>
+            !tile.HasTile || !Main.tileSolid[tile.TileType];//may need Main.tileSolidTop[type]
+
+        const int minLiquid = 32;
+        public bool CrawlWorld(Point16 start)
+        {
+            Tile startTile = Main.tile[start.X, start.Y];
+
+            //liquid type check will be replaced later with different fishing types
+            if (startTile.LiquidAmount > minLiquid && startTile.LiquidType == LiquidID.Water)
+            {
+                Point16 surfaceStart = start;
+                bool atSurface = false;
+
+                //distance to check until surface
+                int surfaceTryDistance = 32;//make const
+                int xRoofOffset = 0;
+                for (int i = 0; i < surfaceTryDistance; i++)
+                {
+                    Tile aboveTile = Main.tile[start.X + xRoofOffset, start.Y - i];
+                    //checks for any passable block (water blocking does not matter)
+                    if (PassThough(aboveTile))//if above does not have a tile, check for liquid. 
+                    {
+                        if (aboveTile.LiquidAmount < minLiquid)//if there is no liquid break loop, else keep trying
+                        {
+                            surfaceStart = new Point16(start.X + xRoofOffset, start.Y - i);
+                            atSurface = true;
+                            break;
+                        }
+                    }
+                    else//if above has tile, check for open spaces nearby
+                    {
+                        int xRoof1 = xRoofOffset - 2;
+                        int xRoof2 = xRoofOffset + 3;//sets new bounds
+                        for (int h = xRoof1; h < xRoof2; h++)//checks in a 5 block area around the previous offset for open tiles
+                        {
+                            xRoofOffset = h;//sets a new offset, if a valid tile is found this offset is kept
+                            aboveTile = Main.tile[start.X + xRoofOffset, start.Y - i];
+                            //checks for any passable block (water blocking does not matter)
+                            if (PassThough(aboveTile))
+                            {
+                                if (aboveTile.LiquidAmount < minLiquid)
+                                {
+                                    surfaceStart = new Point16(start.X + xRoofOffset, start.Y - i);
+                                    atSurface = true;
+                                }
+
+                                //break either way, since if there is a valid gap we keep checking, or if we found the surface we break the outer loop
+                                break;
+                            }
+                        }
+                        if (atSurface)
+                            break;
+                    }
+                }
+
+                if (!atSurface)
+                    return false;//may return null and/or give too deep message
+
+                Point16 groundPos = Point16.Zero;
+                int tryDistance = 100;//make const
+                for (int i = 0; i < tryDistance; i++)
+                {
+                    Tile leftTile = Main.tile[surfaceStart.X - i, start.Y];//start checking if a tile exists without passthough
+                    //checks for any water blocking block (water blocking non-solid should either be counted as solid or throw like a missing water would)
+                }
+            }
+            else
+                return false;//may return null instead
+        }
+
         public void CaptureWorldArea(Point16 point1, Point16 point2)
         {
             int width = Math.Abs(point1.X - point2.X);
@@ -44,6 +115,11 @@ namespace SuperUltraFishing
             Vector3 center = new Vector3((AreaSizeX / 2) - (width / 2), (AreaSizeY / 2) - (height / 2), AreaSizeZ / 2);
             for (int i = 0; i < width; i++)
             {
+                bool rowHasTile = false;
+                bool hasgap = false;
+                Point16 firstTile = Point16.Zero;
+                Point16 secondTile = Point16.Zero;
+
                 for (int j = 0; j < height; j++)
                 {
                     int xoffset = (int)center.X + i;
@@ -51,6 +127,12 @@ namespace SuperUltraFishing
                     int zoffset = (int)center.Z;
 
                     Tile vanillaTile = Main.tile[point1.X + i, point2.Y - j];
+
+                    if (vanillaTile.HasTile)
+                        rowHasTile = true;
+                    else
+                        hasgap = true;
+
                     AreaArray[xoffset, yoffset, zoffset] = new BasicTile()
                     {
                         Active = vanillaTile.HasTile,
