e5ba37b2621304fb85acd3be83aedec43e03a268
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2023-04-07 23:37:35 -0500

    cleaning collision code

--- Player.cs
+++ Player.cs
@@ -130,109 +130,55 @@ namespace SuperUltraFishing
 
         public void TileCollisions()
         {
-            TestCol(0, 1, 1);
-            TestCol(2, 1, 1);
-            TestCol(1, 0, 1);
-            TestCol(1, 2, 1);
-            TestCol(1, 1, 0);
-            TestCol(1, 1, 2);
+            CollideWithTile(0, 1, 1);
+            CollideWithTile(2, 1, 1);
+            CollideWithTile(1, 0, 1);
+            CollideWithTile(1, 2, 1);
+            CollideWithTile(1, 1, 0);
+            CollideWithTile(1, 1, 2);
         }
-        public void TestCol(int i, int j, int k)
+        public void CollideWithTile(int i, int j, int k)
         {
             int tilePosX = (int)((BoundingSphere.Center.X / 10f) - 0.5f) + i;
-                int tilePosY = (int)((BoundingSphere.Center.Y / 10f) - 0.5f) + j;
-                    int tilePosZ = (int)((BoundingSphere.Center.Z / 10f) - 0.5f) + k;
+            int tilePosY = (int)((BoundingSphere.Center.Y / 10f) - 0.5f) + j;
+            int tilePosZ = (int)((BoundingSphere.Center.Z / 10f) - 0.5f) + k;
 
-                    if (world.ValidTilePos(tilePosX, tilePosY, tilePosZ) && world.TempCollisionType(tilePosX, tilePosY, tilePosZ) == 1)
-                    {
-                        BoundingBox tileBox = new BoundingBox(
-                            new Vector3(
-                                (tilePosX - 0.5f),
-                                (tilePosY - 0.5f),
-                                (tilePosZ - 0.5f)) * 10,
-                            new Vector3(
-                                (tilePosX + 0.5f),
-                                (tilePosY + 0.5f),
-                                (tilePosZ + 0.5f)) * 10);
+            if (world.ValidTilePos(tilePosX, tilePosY, tilePosZ) && world.TempCollisionType(tilePosX, tilePosY, tilePosZ) == 1)
+            {
+                BoundingBox tileBox = new BoundingBox(
+                    new Vector3(
+                        (tilePosX - 0.5f),
+                        (tilePosY - 0.5f),
+                        (tilePosZ - 0.5f)) * 10,
+                    new Vector3(
+                        (tilePosX + 0.5f),
+                        (tilePosY + 0.5f),
+                        (tilePosZ + 0.5f)) * 10);
 
-                        if (tileBox.Contains(BoundingSphere.Center) == ContainmentType.Contains)
-                        {
-                            Main.NewText("Contains");
-                        }
-                        else if (tileBox.Contains(BoundingSphere.Center) == ContainmentType.Intersects)
-                        {
-                            Main.NewText("Itersect", Color.IndianRed);
-                        }
+                if (BoundingSphere.Intersects(tileBox))
+                {
+                    Vector3 pointonbox = ClosestPointOnBox(Position, tileBox);
+                    Vector3 dir = (Position - pointonbox);
+                    Vector3 dirOpposite = Vector3.Normalize(dir) * BoundingSphere.Radius;
+                    //Main.NewText(dir);
+                    Position += dirOpposite - dir;
 
-                        if (BoundingSphere.Intersects(tileBox))
-                        {
-                            //Vector3 dirVector = Vector3.Normalize(BoundingSphere.Center - (new Vector3(tilePosX, tilePosY, tilePosZ) * 10));// * 1.5f;
-                            //Vector3 dirAbsVector = new Vector3(Math.Abs(dirVector.X), Math.Abs(dirVector.Y), Math.Abs(dirVector.Z));
-                            //Vector3 roundedDirVec = //new Vector3(Math.Abs((int)dirVector.X), Math.Abs((int)dirVector.Y), Math.Abs((int)dirVector.Z));
-                            //(dirAbsVector.X > dirAbsVector.Y && dirAbsVector.X > dirAbsVector.Z) ? new Vector3(1, 0, 0) :
-                            //(dirAbsVector.Y > dirAbsVector.X && dirAbsVector.Y > dirAbsVector.Z) ? new Vector3(0, 1, 0) :
-                            //(dirAbsVector.Z > dirAbsVector.Y && dirAbsVector.Z > dirAbsVector.X) ? new Vector3(0, 0, 1) :
-                            //Vector3.One;
-
-                            //Main.NewText("|rounded|" + roundedDirVec);
-
-                            //Vector3 offset = (BoundingSphere.Center - (new Vector3(tilePosX, tilePosY, tilePosZ) * 10));
-                            //Vector3 roundedOffset = new Vector3(MathF.Round(offset.X), MathF.Round(offset.Y), MathF.Round(offset.Z));
-
-                            //Vector3 output = roundedDirVec * (offset - roundedOffset);
-                            //Main.NewText("|out|" + output);
-                            //Main.NewText(dirAbsVector);
-                            //Position -= output;
-                            //Position = (dirAbsVector.X >= dirAbsVector.Y && dirAbsVector.X >= dirAbsVector.Z) ? new Vector3(OldPosition.X, Position.Y, Position.Z) :
-                            //(dirAbsVector.Y >= dirAbsVector.X && dirAbsVector.Y >= dirAbsVector.Z) ? new Vector3(Position.X, OldPosition.Y, Position.Z) :
-                            //(dirAbsVector.Z >= dirAbsVector.Y && dirAbsVector.Z >= dirAbsVector.X) ? new Vector3(Position.X, Position.Y, OldPosition.Z) :
-                            //OldPosition;
-                            Vector3 pointonbox = ClosestPointOnBox(Position, tileBox);
-                            Vector3 dir = (Position - pointonbox);
-                            Vector3 dirOpposite = Vector3.Normalize(dir) * BoundingSphere.Radius;
-                            Main.NewText(dir);
-                            Position += dirOpposite - dir;
-
-                            //var axis = a(OldPosition, tileBox);
-                            //if (axis.x)
-                            //    Position.X = OldPosition.X;
-                            //if (axis.y)
-                            //    Position.Y = OldPosition.Y;
-                            //if (axis.z)
-                            //    Position.Z = OldPosition.Z;
-
-
-                            Velocity *= 0.75f;
-                            //Velocity = Velocity - (output * 1f);
-                        }
-                        //Matrix ScalePosBounds = Matrix.CreateScale(1.001f) * Matrix.CreateTranslation(
-                        //        new Vector3(
-                        //        tilePosX * 10,
-                        //        tilePosY * 10,
-                        //        tilePosZ * 10));
-                        //rendering.DebugCube.Draw(rendering.WorldMatrix * ScalePosBounds, rendering.ViewMatrix, rendering.ProjectionMatrix);
-                    }
+                    Velocity *= 0.75f;//may need to be changed
+                }
+            }
         }
 
-        public Vector3 ClosestPointOnBox(Vector3 point, BoundingBox box)
+        public static Vector3 ClosestPointOnBox(Vector3 point, BoundingBox box)
         {
             return new Vector3(Math.Clamp(point.X, box.Min.X, box.Max.X),
                 Math.Clamp(point.Y, box.Min.Y, box.Max.Y),
                 Math.Clamp(point.Z, box.Min.Z, box.Max.Z));
         }
 
-        public (bool x, bool y, bool z) a(Vector3 point, BoundingBox box)
-        {
-            (bool x, bool y, bool z) result = new();
-            result.x = (point.X >= box.Max.X) || (point.X <= box.Min.X);
-            result.y = (point.Y >= box.Max.Y) || (point.Y <= box.Min.Y);
-            result.z = (point.Z >= box.Max.Z) || (point.Z <= box.Min.Z);
-
-            return result;
-        }
-
         public void DrawPlayer()
         {
+
+            //collision draw
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
