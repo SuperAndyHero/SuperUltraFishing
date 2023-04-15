cb535235b428bef0f3c66790b8370f538637412b
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2023-04-15 04:11:32 -0500

    testing triangle collision

--- Player.cs
+++ Player.cs
@@ -18,6 +18,8 @@ using Terraria.ModLoader;
 using Terraria.UI;
 using static Terraria.ModLoader.ModContent;
 using System.Security.Cryptography.X509Certificates;
+using System.Numerics;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
 
 namespace SuperUltraFishing
 {
@@ -37,6 +39,7 @@ namespace SuperUltraFishing
         private FishingUIWindow fishingUIWindow;
 
         public BoundingSphere BoundingSphere;
+        //public BoundingBox BoundingBox;
 
         public bool ShouldUpdate => fishingUIWindow.WindowActive;
         public bool DebugMode => fishingUIWindow.DebugMode;
@@ -51,6 +54,7 @@ namespace SuperUltraFishing
         public void Reset()
         {
             BoundingSphere = new BoundingSphere();
+            //BoundingBox = new BoundingBox();
             Position = new Vector3(world.AreaSizeX, world.AreaSizeY, world.AreaSizeZ) * 4;
             Velocity = Vector3.Zero;
             Yaw = 0;
@@ -112,32 +116,13 @@ namespace SuperUltraFishing
                     Velocity.Y -= SinkSpeed;
 
                 {
-                    Position.X += Velocity.X;
+                    Position += Velocity;
 
                     int debugBoxsize = 4;
                     //sets bounding box to position
+                    Vector3 off = new Vector3(debugBoxsize);
                     BoundingSphere = new BoundingSphere(Position, debugBoxsize);
-
-                    TileCollisions();
-                }
-
-                {
-                    Position.Y += Velocity.Y;
-
-                    int debugBoxsize = 4;
-                    //sets bounding box to position
-                    BoundingSphere = new BoundingSphere(Position, debugBoxsize);
-
-                    TileCollisions();
-                }
-
-                {
-                    Position.Z += Velocity.Z;
-
-                    int debugBoxsize = 4;
-                    //sets bounding box to position
-                    BoundingSphere = new BoundingSphere(Position, debugBoxsize);
-
+                    //BoundingBox = new BoundingBox(Position - off, Position + off);
                     TileCollisions();
                 }
 
@@ -165,46 +150,47 @@ namespace SuperUltraFishing
             //CollideWithTile(1, 1, 0);
             //CollideWithTile(1, 1, 2);
             //Vector3 MaxVec = Vector3.Zero;
-            float MaxVecLength = 0;
-            float AvrVecLength = 0;
+            //float MaxVecLength = 0;
+            //float AvrVecLength = 0;
 
-            foreach (var vec in TotalDirList)
-            {
-                TotalDir += vec;
-                float veclen = vec.Length();
-                AvrVecLength += veclen / TotalDirList.Count;
-                if (veclen > MaxVecLength)
-                {
-                    MaxVecLength = veclen;
-                    //MaxVec = vec;
-                }
-            }
-            Vector3 norDir = TotalDir == Vector3.Zero ? Vector3.Zero : Vector3.Normalize(TotalDir);
+            //foreach (var vec in TotalDirList)
+            //{
+            //    TotalDir += vec;
+            //    float veclen = vec.Length();
+            //    AvrVecLength += veclen / TotalDirList.Count;
+            //    if (veclen > MaxVecLength)
+            //    {
+            //        MaxVecLength = veclen;
+            //        //MaxVec = vec;
+            //    }
+            //}
+            //Vector3 norDir = TotalDir == Vector3.Zero ? Vector3.Zero : Vector3.Normalize(TotalDir);
 
-            Position += norDir * MaxVecLength;
+            //Position += norDir * MaxVecLength;
 
-            TotalDir = Vector3.Zero;
-            TotalDirList.Clear();
+            //TotalDir = Vector3.Zero;
+            //TotalDirList.Clear();
         }
         public Vector3 TotalDir = Vector3.Zero;
         public List<Vector3> TotalDirList = new List<Vector3>();
         public void CollideWithTile(int i, int j, int k)
         {
-            int tilePosX = (int)((BoundingSphere.Center.X / 10f) - 0.5f) + i;
-            int tilePosY = (int)((BoundingSphere.Center.Y / 10f) - 0.5f) + j;
-            int tilePosZ = (int)((BoundingSphere.Center.Z / 10f) - 0.5f) + k;
+            int tilePosX = (int)((Position.X / 10f) - 0.5f) + i;
+            int tilePosY = (int)((Position.Y / 10f) - 0.5f) + j;
+            int tilePosZ = (int)((Position.Z / 10f) - 0.5f) + k;
 
             if (world.ValidTilePos(tilePosX, tilePosY, tilePosZ) && world.TempCollisionType(tilePosX, tilePosY, tilePosZ) == 1)
             {
-                BoundingBox tileBox = new BoundingBox(
-                    new Vector3(
-                        (tilePosX - 0.5f),
-                        (tilePosY - 0.5f),
-                        (tilePosZ - 0.5f)) * 10,
-                    new Vector3(
-                        (tilePosX + 0.5f),
-                        (tilePosY + 0.5f),
-                        (tilePosZ + 0.5f)) * 10);
+                //    BoundingBox tileBox = new BoundingBox(
+                //        new Vector3(
+                //            (tilePosX - 0.5f),
+                //            (tilePosY - 0.5f),
+                //            (tilePosZ - 0.5f)) * 10,
+                //        new Vector3(
+                //            (tilePosX + 0.5f),
+                //            (tilePosY + 0.5f),
+                //            (tilePosZ + 0.5f)) * 10);
+
 
                 //if (BoundingSphere.Intersects(tileBox))
                 //{
@@ -215,28 +201,141 @@ namespace SuperUltraFishing
                 //    Main.NewText("Itersect", Color.IndianRed);
                 //}
 
-                //clips into corners because block isnt checked
-                if (BoundingSphere.Intersects(tileBox))
+                void CheckTriangle(Triangle triangle)
                 {
-                    Main.NewText("Contains");
-                    Vector3 pointonbox = ClosestPointOnBox(Position, tileBox);
-                    Vector3 dir = (Position - pointonbox);
-                    Vector3 dirOpposite = Vector3.Normalize(dir) * BoundingSphere.Radius;
-                    //Main.NewText(dir);
-                    TotalDirList.Add(dirOpposite - dir);
-                    //TotalDir += dirOpposite - dir;
+                    //clips into corners because block isnt checked
+                    if (SphereIntersectsTriangle(BoundingSphere, triangle, out Vector3 closestPointOnTri))
+                    {
+                        Main.NewText("Contains");
 
-                    Velocity *= 1f;// 0.75f;//may need to be changed
+                        Position += triangle.Normal * 
+                            (Vector3.Distance(BoundingSphere.Center, closestPointOnTri) - BoundingSphere.Radius);
+
+
+                        //Main.NewText(dir);
+                        //TotalDirList.Add(dirOpposite - dir);
+                        //TotalDir += dirOpposite - dir;
+
+                        Velocity *= 0.5f;// 0.75f;//may need to be changed
+                    }
                 }
+
+                //up, 1
+                Triangle triangleUp1 = new Triangle(
+                        new Vector3(
+                            (tilePosX - 0.5f),
+                            (tilePosY + 0.5f),
+                            (tilePosZ - 0.5f)) * 10,
+                        new Vector3(
+                            (tilePosX - 0.5f),
+                            (tilePosY + 0.5f),
+                            (tilePosZ + 0.5f)) * 10,
+                        new Vector3(
+                            (tilePosX + 0.5f),
+                            (tilePosY + 0.5f),
+                            (tilePosZ - 0.5f)) * 10);
+
+                CheckTriangle(triangleUp1);
+
+                Triangle triangleUp2 = new Triangle(
+                        new Vector3(
+                            (tilePosX + 0.5f),
+                            (tilePosY + 0.5f),
+                            (tilePosZ + 0.5f)) * 10,
+                        new Vector3(
+                            (tilePosX + 0.5f),
+                            (tilePosY + 0.5f),
+                            (tilePosZ - 0.5f)) * 10,
+                        new Vector3(
+                            (tilePosX - 0.5f),
+                            (tilePosY + 0.5f),
+                            (tilePosZ + 0.5f)) * 10);
+
+                CheckTriangle (triangleUp2);
             }
         }
 
-        public static Vector3 ClosestPointOnBox(Vector3 point, BoundingBox box)
+        public struct Triangle
         {
-            return new Vector3(Math.Clamp(point.X, box.Min.X, box.Max.X),
-                Math.Clamp(point.Y, box.Min.Y, box.Max.Y),
-                Math.Clamp(point.Z, box.Min.Z, box.Max.Z));
+            public Vector3 PointA;
+            public Vector3 PointB;
+            public Vector3 PointC;
+
+            public Vector3 Normal;
+
+            public Triangle(Vector3 PointA, Vector3 PointB, Vector3 PointC)
+            {
+                this.PointA = PointA; 
+                this.PointB = PointB;
+                this.PointC = PointC;
+                Normal = Rendering.CalculateNormal(PointA, PointB, PointC);
+            }
         }
+        public bool SphereIntersectsTriangle(BoundingSphere sphere, Triangle triangle, out Vector3 point) =>
+            SphereIntersectsTriangle(sphere, triangle.PointA, triangle.PointB, triangle.PointC, out point);
+        public bool SphereIntersectsTriangle(BoundingSphere sphere, Vector3 a, Vector3 b, Vector3 c, out Vector3 point)
+        {
+            // Find point P on triangle ABC closest to sphere center
+            point = ClosestPointOnTriangle(sphere.Center, a, b, c);
+            // Sphere and triangle intersect if the (squared) distance from sphere
+            // center to point p is less than the (squared) sphere radius
+            Vector3 v = point - sphere.Center;
+            return Vector3.Dot(v, v) <= sphere.Radius * sphere.Radius;
+        }
+        public Vector3 ClosestPointOnTriangle(Vector3 point, Vector3 a, Vector3 b, Vector3 c)
+        {
+            // Check if P in vertex region outside A
+            Vector3 ab = b - a;
+            Vector3 ac = c - a;
+            Vector3 ap = point - a;
+            float d1 = Vector3.Dot(ab, ap);
+            float d2 = Vector3.Dot(ac, ap);
+            if (d1 <= 0.0f && d2 <= 0.0f) return a; // barycentric coordinates (1,0,0)
+                                                    // Check if P in vertex region outside B
+            Vector3 bp = point - b;
+            float d3 = Vector3.Dot(ab, bp);
+            float d4 = Vector3.Dot(ac, bp);
+            if (d3 >= 0.0f && d4 <= d3) return b; // barycentric coordinates (0,1,0)
+                                                  // Check if P in edge region of AB, if so return projection of P onto AB
+            float vc = d1 * d4 - d3 * d2;
+            if (vc <= 0.0f && d1 >= 0.0f && d3 <= 0.0f)
+            {
+                float vd = d1 / (d1 - d3);
+                return a + vd * ab; // barycentric coordinates (1-v,v,0)
+            }
+            // Check if P in vertex region outside C
+            Vector3 cp = point - c;
+            float d5 = Vector3.Dot(ab, cp);
+            float d6 = Vector3.Dot(ac, cp);
+            if (d6 >= 0.0f && d5 <= d6) return c; // barycentric coordinates (0,0,1)
+
+            // Check if P in edge region of AC, if so return projection of P onto AC
+            float vb = d5 * d2 - d1 * d6;
+            if (vb <= 0.0f && d2 >= 0.0f && d6 <= 0.0f)
+            {
+                float wd = d2 / (d2 - d6);
+                return a + wd * ac; // barycentric coordinates (1-w,0,w)
+            }
+            // Check if P in edge region of BC, if so return projection of P onto BC
+            float va = d3 * d6 - d5 * d4;
+            if (va <= 0.0f && (d4 - d3) >= 0.0f && (d5 - d6) >= 0.0f)
+            {
+                float wf = (d4 - d3) / ((d4 - d3) + (d5 - d6));
+                return b + wf * (c - b); // barycentric coordinates (0,1-w,w)
+            }
+            // P inside face region. Compute Q through its barycentric coordinates (u,v,w)
+            float denom = 1.0f / (va + vb + vc);
+            float v = vb * denom;
+            float w = vc * denom;
+            return a + ab * v + ac * w; // = u*a + v*b + w*c, u = va * denom = 1.0f-v-w
+        }
+
+        //public static Vector3 ClosestPointOnBox(Vector3 point, BoundingBox box)
+        //{
+        //    return new Vector3(Math.Clamp(point.X, box.Min.X, box.Max.X),
+        //        Math.Clamp(point.Y, box.Min.Y, box.Max.Y),
+        //        Math.Clamp(point.Z, box.Min.Z, box.Max.Z));
+        //}
 
         public void DrawPlayer()
         {
@@ -254,9 +353,9 @@ namespace SuperUltraFishing
 
         public void DrawDebugTileBox(int i, int j, int k)
         {
-            int tilePosX = (int)((BoundingSphere.Center.X / 10f) - 0.5f) + i;
-            int tilePosY = (int)((BoundingSphere.Center.Y / 10f) - 0.5f) + j;
-            int tilePosZ = (int)((BoundingSphere.Center.Z / 10f) - 0.5f) + k;
+            int tilePosX = (int)((Position.X / 10f) - 0.5f) + i;
+            int tilePosY = (int)((Position.Y / 10f) - 0.5f) + j;
+            int tilePosZ = (int)((Position.Z / 10f) - 0.5f) + k;
 
             if (world.ValidTilePos(tilePosX, tilePosY, tilePosZ) && world.TempCollisionType(tilePosX, tilePosY, tilePosZ) == 1)
             {
